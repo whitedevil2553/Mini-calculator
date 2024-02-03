aac055ed9ef7aa7548bc994c11bef95b3a7ce732
RAGAV <113360112+whitedevil2553@users.noreply.github.com>
2024-02-03 12:56:17 +0530

    Mini-calculator

---
+++ Mini-calculator/main.js
@@ -0,0 +1,14 @@
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+function calculate() {
+    try {
+        document.getElementById('display').value = eval(document.getElementById('display').value);
+    }
+    catch(e) {
+        document.getElementById('display').value = 'ERROR';
+    }
+}
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
